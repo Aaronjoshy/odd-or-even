15c32590d4e22439cb660d171f46bdbac1288c5c
Aaron E J <120198986+Aaronjoshy@users.noreply.github.com>
2024-03-14 10:44:58 +0530

    Update oddorev.js

--- oddorev.js
+++ oddorev.js
@@ -1,9 +1,8 @@
-document.write("<h2>Odd or Even Numbers from 0 to 15:</h2>");
 
 for (let i = 0; i <= 15; i++) {
     if (i % 2 === 0) {
-        document.write(i + " is even<br>");
+        document.write(i + " is even");
     } else {
-        document.write(i + " is odd<br>");
+        document.write(i + " is odd");
     }
 } 
